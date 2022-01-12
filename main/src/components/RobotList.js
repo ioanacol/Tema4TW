623ad9c8a323ea1ff408d66f37202abc83be2c5b
Ioana Colceag <ioana.kol1101@gmail.com>
2022-01-13 01:23:18 +0200

    second commit

--- main/src/components/RobotList.js
+++ main/src/components/RobotList.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
-import RobotStore from '../stores/RobotStore'
-import Robot from './Robot'
-import RobotForm from './RobotForm'
-
-
+import React, { Component } from "react";
+import RobotStore from "../stores/RobotStore";
+import Robot from "./Robot";
+import RobotForm from "./RobotForm";
 
 class RobotList extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots : []
-		}
-	}
-	componentDidMount(){
-		this.store = new RobotStore()
-		this.setState({
-			robots : this.store.getRobots()
-		})
-		this.store.emitter.addListener('UPDATE', () => {
-			this.setState({
-				robots : this.store.getRobots()
-			})			
-		})
-	}
-	render() {
-		return (
-			<div>
-				 
-				{
-					this.state.robots.map((e, i) => 
-						<Robot item={e} key={i} />
-					)
-				}
-			</div>
-		)
-	}
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+    };
+  }
+  componentDidMount() {
+    this.store = new RobotStore();
+    this.setState({
+      robots: this.store.getRobots(),
+    });
+    this.store.emitter.addListener("UPDATE", () => {
+      this.setState({
+        robots: this.store.getRobots(),
+      });
+    });
+  }
+
+  onAdd(newRobot) {
+    this.store.addRobot(newRobot);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.robots.map((e, i) => (
+          <Robot item={e} key={i} />
+        ))}
+        <RobotForm onAdd={this.onAdd} store={this.store} />
+      </div>
+    );
+  }
 }
 
-export default RobotList
+export default RobotList;
